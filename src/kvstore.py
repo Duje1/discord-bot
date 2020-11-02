d359b3dea27043fce7fed8029b4e7bff8caf1f96
Dillon Davis <07734willy@users.noreply.github.com>
2020-11-02 12:01:39 -0600

    added kvstore / persistent dict

---
+++ src/kvstore.py
@@ -0,0 +1,26 @@
+from collections import UserDict
+import pickle
+import os
+
+class KVStore(UserDict):
+	def __init__(self, path):
+		super().__init__()
+		self.path = path
+		self.read()
+
+	def __setitem__(self, key, value):
+		super().__setitem__(key, value)
+		self.write()
+
+	def read(self):
+		if os.path.exists(self.path):
+			with open(self.path, 'rb') as f:
+				self.data = pickle.load(f)
+
+	def write(self):
+		with open(self.path, 'wb') as f:
+			pickle.dump(self.data, f)
+
+# example usage
+# messages = KVStore('./tables/role-msgs.pkl')
+# role = messages[msg_id]
